8d9564023f40dde9ae9ecba1080140a51ffd7cf2
Hunter Lannon <kindlehl@osuosl.org>
2019-03-29 02:31:43 -0700

    dropped test yaml path

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import remote_execution
 args = cli_parser.get()
 
 # Loads yml configuration. Without path argument, defaults to ~/.commander.yml
-yaml = conf.YAMLoader(path='local.yml')
+yaml = conf.YAMLoader()
 targets = target.createTargetsFromYaml(yaml)
 
 for t in targets:
