0a6c5ab8bd7170abbf02fbdd1f03173f49e50b89
Hunter Lannon <kindlehl@osuosl.org>
2019-03-31 00:15:28 -0700

    changed targets to target. Changed from list to hash

--- shelli/target.py
+++ shelli/target.py
@@ -17,21 +17,22 @@ from shelli import hostgroup
 # Targets are READ ONLY
 # Do NOT modify anything in them. This way, you won't need to make copies of everything!!!
 class Target:
-    """Class to represent a target. Created with a yaml object"""
+    """Class to represent a target. Created with a yaml object and a list of hostgroups"""
 
     def __init__(self, name, all_hostgroups, target_yaml):
         self.name = name
         self.commands = target_yaml['commands']
         self.hostgroup_names = target_yaml['hostgroups']
+
         if 'transport' in list(target_yaml.keys()):
             self.transports = target_yaml['transport']
         else:
             self.transports = []
 
-        self.hostgroups = []
-        for group in all_hostgroups:
+        self.hostgroups = {}
+        for group in all_hostgroups.values():
             if group.name in self.hostgroup_names:
-                self.hostgroups.append(group)
+                self.hostgroups[group.name] = group
 
     def __str__(self):
         return self.name
@@ -42,20 +43,22 @@ class Target:
     def get_all_hosts(self):
         """Returns list of all hosts on target."""
 
-        all_hosts = []
-        for group in self.hostgroups:
-            for host in group.hosts:
-                all_hosts.append(host)
-        return all_hosts
+        all_hosts = {}
+        for group in self.hostgroups.values():
+            for host_key in group.hosts:
+                all_hosts[host_key] = group.hosts[host_key]
 
+        return all_hosts
 
 def create_targets_from_yaml(yaml):
     """Creates a list of all targets, given a yaml object"""
 
     all_hostgroups = hostgroup.create_host_groups_from_yaml(yaml)
-    targets = []
+    targets = {}
+
     for target in yaml['targets']:
         target_name = list(target.keys())[0]
         new_target = Target(target_name, all_hostgroups, target[target_name])
-        targets.append(new_target)
+        targets[target_name] = new_target
+
     return targets
