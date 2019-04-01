f4c97641152de05b666a956bea7bd3ea1d7d4599
Hunter Lannon <kindlehl@osuosl.org>
2019-03-31 17:01:31 -0700

    added output for commands

--- shelli/execute.py
+++ shelli/execute.py
@@ -3,6 +3,8 @@ Class for executing commands. Uses dependency injection
 to run commands from a target.
 """
 
+import sys
+
 from shelli import authenticate
 from shelli import transport
 
@@ -13,6 +15,12 @@ class Executor:
     def __init__(self, target):
         """Nothing."""
         self.hosts = target.get_all_hosts()
+
+        self.longest_host_string = 0
+        for host in self.hosts:
+            if len(str(self.hosts[host])) > self.longest_host_string:
+                self.longest_host_string = len(str(self.hosts[host]))
+
         self.commands = target.commands
         self.transporters = []
         for transport_string in target.transports:
@@ -24,21 +32,31 @@ class Executor:
         # Get all connections. Will probably require authentication.
         # This way makes sure all hosts get authentication information up-front
         # Instead of 5 minutes later, after running a long command
-        conns = []
+        conns = {}
         for cur_host in self.hosts.values():
-            print("Creating connection {name}".format(name=cur_host.hostname))
-            conns.append(authenticate.get_connection(cur_host))
+            print("Creating connection {name}".format(name=cur_host))
+            conns[str(cur_host)] = authenticate.get_connection(cur_host)
 
         for conn in conns:
             # Transport any necessary files
-            self.do_transport(conn)
+            self.do_transport(conns[conn])
 
             # Run commands
-            for command in self.commands:
-                conn.run(command)
+            self.run(conn, self.commands, conns[conn])
 
             # Clean up, ya filthy animal
-            self.cleanup(conn)
+            self.cleanup(conns[conn])
+
+    def run(self, message, commands, connection):
+        """Runs a command on a given connection"""
+        for command in commands:
+            prompt_header = "[{}]".format(message)
+            format_string = "{: <" + str(self.longest_host_string + 2) + "}"
+            prompt_header = format_string.format(prompt_header)
+            prompt_body = "\tRunning '{}'\n".format(command)
+            sys.stderr.write(prompt_header + prompt_body)
+            sys.stderr.flush()
+            connection.run(command)
 
     def do_transport(self, connection):
         """Move files to remote"""
