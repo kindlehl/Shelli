fb9c04702a6f626b0664f217c92df06ad7aef19f
Hunter Lannon <kindlehl@osuosl.org>
2019-03-31 23:52:21 -0700

    Bump to version 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Shelli',
-    version='0.2',
+    version='0.3',
     description=open('README.md').read(),
     url='https://github.com/kindlehl/shelli',
     author='Hunter Lannon',
